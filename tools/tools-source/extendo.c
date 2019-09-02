23cd49f8d8efd5bdc4dc30d67149acfb4e0b9eed
Nathan Wichers <bluroseai@hotmail.com>
2019-09-02 13:29:44 -0400

    fix up extendo & new binary
    make it build sans-error
    also finally got code::blocks setup so let's get this bread

--- tools/tools-source/extendo.c
+++ tools/tools-source/extendo.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define INTEL_FIX  /*kill this if you're reading stuff in without swapping*/
 
 /*standard 32bit byteswapping*/
 unsigned long byteswap(unsigned long w)
 {
     return (w >> 24) | ((w >> 8) & 0x0000ff00) | ((w << 8) & 0x00ff0000) | (w << 24);
-}    
+}
 
 /*standard 16bit byteswapping*/
 unsigned short int shortswap(unsigned short int w)
@@ -23,23 +24,23 @@ int main(int argc, char *argv[])
     unsigned long x, buf, size;
     unsigned short z;
 
-    if (argc > 1) 
+    if (argc > 1)
         strcpy(filename, argv[argc-1]);
 
     while (!(in = fopen(filename, "rb")))
     {
 	    printf("\nN64 ROM? ");
-	    strcpy(filename,"\0");	
+	    strcpy(filename,"\0");
 	    gets(filename);
     }
 
     strtok(filename,".");
     strcat(filename, "-ext.n64");
 
-    while (!(out = fopen(filename, "wb"))) 
+    while (!(out = fopen(filename, "wb")))
     {
         printf("\nextended filename? ");
-        strcpy(filename,"\0");	
+        strcpy(filename,"\0");
         gets(filename);
     }
 
@@ -50,23 +51,23 @@ int main(int argc, char *argv[])
     /*check swapping on file using first word*/
     rewind(in);
     fread(&x, 4, 1, in);
-    
+
     #ifdef INTEL_FIX
     x = byteswap(x);
     #endif
-    
+
     if (x == 0x80371240) /*no swapping*/
         y = 1;
     else if (x == 0x37804012) /*16bit byteswapped*/
         y = 2;
     else if (x == 0x40123780) /*32bit byteswapped*/
         y = 4;
-    
-    if (y > 1) 
+
+    if (y > 1)
     {
         printf("\a\nThe bugger is %sbit byteswapped.\nShould I straighten it out? (Y/N)\t", y > 2 ? "32":"16");
         x = getchar();
-        if (x !='Y' && x != 'y') 
+        if (x !='Y' && x != 'y')
             y=1;
     }
 
@@ -75,20 +76,20 @@ int main(int argc, char *argv[])
     for (x = 0; x < size; x += 4)
     {
         fseek(in, x, SEEK_SET);
-    
-        if(y == 2) 
+
+        if(y == 2)
         {
-            fread(&z, 2, 1, in); 
-            z = shortswap(z); 
+            fread(&z, 2, 1, in);
+            z = shortswap(z);
             fwrite(&z, 2, 1, out);
-            fread(&z, 2, 1, in); 
-            z = shortswap(z); 
+            fread(&z, 2, 1, in);
+            z = shortswap(z);
             fwrite(&z, 2, 1, out);
         }
-        else 
+        else
         {
             fread(&buf, 4, 1, in);
-            if (y > 1) 
+            if (y > 1)
                 buf = byteswap(buf);
             fwrite(&buf, 4, 1, out);
         }
@@ -99,18 +100,18 @@ int main(int argc, char *argv[])
     fflush(out);
     x = size >> 17;     /*convert size to Mb*/
 
-    if(x > 0x100) 
+    if(x > 0x100)
         printf("\a\n512Mb is the normal maximum rom size.\nDo you want to kick it up from %iMb to %iMb? (Y/N)\t", x, x<<1);
-    else 
+    else
         printf("\a\nShould I extend the rom from %iMb to %iMb? (Y/N)\t", x, x<<1);
-    
+
     y = getchar();
 
     if(y == 'Y' || y == 'y')
     {
         buf=0; fseek(out,0,SEEK_END);
         printf("\nExtending file...    ");
-        for(x = 0; x < size; x += 4) 
+        for(x = 0; x < size; x += 4)
             fwrite(&buf, 4, 1, out);
     }
     /*end extendo*/
