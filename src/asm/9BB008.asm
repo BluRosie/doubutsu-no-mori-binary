4c5d4192dbda87b5327b23d8332dc8da17f9380e
Nathan Wichers <bluroseai@hotmail.com>
2020-12-28 02:48:30 -0500

    var width fonts v2
    now much cleaner
    
    also flex the translation of the quest message in the package stuff

---
+++ src/asm/9BB008.asm
@@ -0,0 +1,8 @@
+.n64
+.create "build/yaz0/9BB008.yaz0", 0 // circumvent yaz0 compression
+
+// this file takes care of the font graphics.  shifting them over so that the variable width table can take over.
+
+.import "src/data/9BB008-fontdata.bin"
+
+.close
